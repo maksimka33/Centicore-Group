a2b30273bf6689227853a06c27ddb0be6fac5e1a
Sergey <112411074+maksimka33@users.noreply.github.com>
2023-07-06 10:52:31 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -70,4 +70,23 @@ noUiSlider.create(arbitraryValuesSlider, {
    pips: { mode: "steps", format: format, density: 50 },
 });
 
-/*  ===================== FORM ===================== */
+/*  ===================== NAV-ICON ===================== */
+
+const navBtn = document.querySelector(".nav-icon-btn");
+const nav = document.querySelector(".mobile-nav");
+const navIcon = document.querySelector(".nav-icon");
+const fade = document.querySelector(".mobile-nav-fade");
+
+navBtn.onclick = function () {
+   nav.classList.toggle("mobile-nav--open");
+   navIcon.classList.toggle("nav-icon--active");
+   fade.classList.toggle("mobile-nav-fade--open");
+   document.body.classList.toggle("no-scroll");
+};
+
+fade.onclick = function () {
+   nav.classList.toggle("mobile-nav--open");
+   fade.classList.toggle("mobile-nav-fade--open");
+   navIcon.classList.toggle("nav-icon--active");
+   document.body.classList.toggle("no-scroll");
+};
